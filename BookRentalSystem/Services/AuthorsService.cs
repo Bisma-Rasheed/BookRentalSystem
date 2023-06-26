2e11fc113ea152ca5eabb22468928199af7fadd3
Bisma Rasheed <53636879+Bisma-Rasheed@users.noreply.github.com>
2023-06-26 10:58:05 +0500

    minor changes

--- BookRentalSystem/Services/AuthorsService.cs
+++ BookRentalSystem/Services/AuthorsService.cs
@@ -14,17 +14,6 @@ namespace BookRentalSystem.Services
         {
             _unitOfWork = unitOfWork;
         }
-        //public async Task<IEnumerable<Author>> GetAllAuthors()
-        //{
-        //    var result = await _unitOfWork.AuthorRepository.GetAll();
-        //    return result;
-        //}
-
-        //public async Task<Author> GetAuthor(int id)
-        //{
-        //    var result = await _unitOfWork.AuthorRepository.GetById(id);
-        //    return result;
-        //}
         public async Task<Author> AddAuthor(AuthorDTO authorDTO)
         {
             var result = await _unitOfWork.AuthorRepository.AddAuthor(authorDTO);
@@ -35,22 +24,6 @@ namespace BookRentalSystem.Services
             await _unitOfWork.AuthorRepository.UpdateAuthor(id, authorDTO);
             return;
         }
-
-        //public async Task DeleteAuthor(int id)
-        //{
-        //    await _unitOfWork.AuthorRepository.RemoveItem(id);
-        //    return;
-        //}
-
-        //public async Task<bool> IfExists(int id)
-        //{
-        //    return await _unitOfWork.AuthorRepository.IfExists(id);
-        //}
-
-        //public bool IfTableExists()
-        //{
-        //    return _unitOfWork.AuthorRepository.IfTableExists();
-        //}
 
        
     }
